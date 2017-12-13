c0119281aac15986638ceec23259607281815527
maciej-wantuch <wantuch.maciej@gmail.com>
2017-12-13 12:56:47 +0100

    Simple mailing added

---
+++ CoreWebCrawler/Mailer.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace CoreWebCrawler
+{
+    public class Mailer
+    {
+        public static void SendMail()
+        {
+            var fromAddress = new MailAddress(MailerConfigurator.GetSenderAddress(), MailerConfigurator.GetSenderName());
+            var toAddress = new MailAddress(MailerConfigurator.GetRecipientAddress(), MailerConfigurator.GetRecipientName());
+            const string subject = "Trinkets List";
+            const string body = "Body";
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = MailerConfigurator.GetSenderServer(),
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, MailerConfigurator.GetSenderPassword()),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
---
+++ CoreWebCrawler/MailerConfigurator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+
+namespace CoreWebCrawler
+{
+    public class MailerConfigurator
+    {
+        private static XmlDocument xDoc = new XmlDocument();
+
+        public static string GetSenderAddress()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("senderAddress").InnerText;
+        }
+
+        public static string GetSenderName()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("senderName").InnerText;
+        }
+
+        public static string GetSenderPassword()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("senderPassword").InnerText;
+        }
+        public static string GetSenderServer()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("senderServer").InnerText;
+        }
+        public static string GetRecipientAddress()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("recipientAddress").InnerText;
+        }
+        public static string GetRecipientName()
+        {
+            xDoc.Load("MailerConfig.xml");
+            return xDoc.DocumentElement.SelectSingleNode("recipientName").InnerText;
+        }
+    }
+}
